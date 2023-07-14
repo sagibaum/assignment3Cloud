edc82f317c7756eb18d41e6ad011bc5832271356
Sagi Baum <sagibaum@gmail.com>
2023-07-14 11:09:22 +0300

    new commit

--- tests/assn3_tests.py
+++ tests/assn3_tests.py
@@ -1,85 +1,64 @@
-import requests
-import pytest
+import connectionController
+from assertions import *
 
-print("Running tests...")
-dish_ids = []
-
-# Test 1: Execute three POST /dishes requests using the dishes "orange", "spaghetti", and "apple pie"
-def test_post_dishes():
+def test_insert_three_dishes():
     dish_names = ["orange", "spaghetti", "apple pie"]
+    dish_ids = []
 
-    for name in dish_names:
-        response = requests.post("http://localhost:8000/dishes", data={"name": name})
+    for dish_name in dish_names:
+        response = connectionController.http_post("dishes", data={'name': dish_name})
         assert response.status_code == 201
-        dish_ids.append(response.json()["id"])
-    assert len(set(dish_ids)) == len(dish_names)
+        dish_ids.append(response.json())
 
-# Test 2: Execute a GET dishes/<orange-ID> request
-def test_get_dish():
-    response = requests.get(f"http://localhost:8000/dishes/{dish_ids[0]}")
-    assert response.status_code == 200
-    dish = response.json()
-    assert 0.9 <= dish["sodium"] <= 1.1
+    assert len(set(dish_ids)) == len(dish_ids)
 
-# Test 3: Execute a GET /dishes request
+def test_get_orange_dish():
+    response = connectionController.http_get("/dishes/1")
+    assert_status_code(response, 200)
+    sodium_field = response.json()['sodium']
+    assert 0.9 <= sodium_field <= 1.1
+
 def test_get_all_dishes():
-    response = requests.get("http://localhost:8000/dishes")
-    assert response.status_code == 200
-    dishes = response.json()
-    assert len(dishes) == 3
+    response = connectionController.http_get("dishes")
+    assert_status_code(response, 200)
+    assert len(response.json()) == 3
 
-# Test 4: Execute a POST /dishes request supplying the dish name "blah"
-def test_post_dish_blah():
-    response = requests.post("http://localhost:8000/dishes", data={"name": "blah"})
-    assert response.status_code in [400, 404, 422]
-    assert response.json()["code"] == -3
+def test_insert_dish_doesnt_exist():
+    dish_name = "blah"
+    response = connectionController.http_post("dishes", data={'name': dish_name})
+    assert_ret_value(response, -3)
+    assert_status_code(response, 404 or 400 or 422)
 
-# Test 5: Perform a POST dishes request with the dish name "orange"
-def test_post_existing_dish():
-    response = requests.post("http://localhost:8000/dishes", data={"name": "orange"})
-    assert response.status_code in [400, 404, 422]
-    assert response.json()["code"] == -2
+def test_insert_dish_already_exists():
+    dish_name = "orange"
+    response = connectionController.http_post("dishes", data={'name': dish_name})
+    assert_ret_value(response, -2)
+    assert_status_code(response, 404 or 400 or 422)
 
-# Test 6: Perform a POST /meals request specifying the meal name as "delicious" and the dish IDs
 def test_post_meal():
     meal_data = {
-        "name": "delicious",
-        "appetizer": dish_ids[0],
-        "main": dish_ids[1],
-        "dessert": dish_ids[2]
+        'name': "delicious",
+        'appetizer': 1,
+        'main': 2,
+        'dessert': 3
     }
+    response = connectionController.http_post("meals", data=meal_data)
+    assert_status_code(response, 201)
+    assert response.json() > 0
 
-    response = requests.post("http://localhost:8000/meals", data=meal_data)
-    assert response.status_code == 201
-    assert response.json()["id"] > 0
-
-# Test 7: Perform a GET /meals request
 def test_get_meals():
-    response = requests.get("http://localhost:8000/meals")
-    assert response.status_code == 200
-    meals = response.json()
-    assert len(meals) == 1
-    assert 400 <= meals[0]["calories"] <= 500
+    response = connectionController.http_get("meals")
+    assert_status_code(response, 200)
+    assert len(response.json()) == 1
+    assert 400 <= response.json()["1"]['cal'] <= 500
 
-# Test 8: Perform a POST /meals request with the same meal name
-def test_post_existing_meal():
+def test_post_meal_already_exists():
     meal_data = {
-        "name": "delicious",
-        "appetizer": dish_ids[0],
-        "main": dish_ids[1],
-        "dessert": dish_ids[2]
+        'name': "delicious",
+        'appetizer': 1,
+        'main': 2,
+        'dessert': 3
     }
-
-    response = requests.post("http://localhost:8000/meals", data=meal_data)
-    assert response.status_code in [400, 422]
-    assert response.json()["code"] == -2
-
-#call all tests
-test_post_dishes()
-test_get_dish()
-test_get_all_dishes()
-test_post_dish_blah()
-test_post_existing_dish()
-test_post_meal()
-test_get_meals()
-test_post_existing_meal()
+    response = connectionController.http_post("meals", data=meal_data)
+    assert_status_code(response, 400 or 422)
+    assert_ret_value(response, -2)
---
+++ tests/connectionController.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from typing import Dict, Any
+
+URL = "http://localhost:8000"
+
+
+def http_get(resource: str):
+    response = requests.get(url=f"{URL}/{resource}", headers={"Content-Type": "application/json"})
+    return response
+
+
+def http_delete(resource: str):
+    response = requests.delete(url=f"{URL}/{resource}", headers={"Content-Type": "application/json"})
+    return response
+
+
+def http_post(resource: str, data: Dict[str, Any]):
+    response = requests.post(url=f"{URL}/{resource}", headers={"Content-Type": "application/json"}, data=json.dumps(data))
+    return response
+
+def http_post_qs(resource: str, query_string: str):
+    response = requests.post(url=f"{URL}/{resource}?name={query_string}", headers={})
+    return response
+
+
+def http_put(resource: str, data: Dict[str, Any]):
+    response = requests.put(url=f"{URL}/{resource}", headers={"Content-Type": "application/json"}, data=json.dumps(data))
+    return response
